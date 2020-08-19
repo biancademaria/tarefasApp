d2f3187988bb519ff96ddf69eb5c21997d32f895
biancademaria <bianca.maria@etec.sp.gov.br>
2020-08-19 15:32:49 -0300

    Formulários e Validações Simples
    Tudo correto, com os formulários e as validações necessárias funcionando

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  public formRegistro: FormGroup;
+
+  public mensagens_validacao = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo nome é obrigatório!' },
+      { tipo: 'minlength', mensagem: 'O campo nome deve ter pelo menos 3 caracteres! '}
+    ],
+    cpf: [
+      { tipo: 'required', mensagem: 'O campo CPF é obrigatório!' }, //obrigatorio 
+      { tipo: 'minlength', mensagem: 'O campo CPF deve ter pelo menos 11 caracteres!' }, //min 11
+      { tipo: 'maxlength', mensagem: 'O campo CPF deve ter no máximo 14 caracteres!' } //max 14
+    ],
+    nascimento: [
+      { tipo: 'required', mensagem: 'O campo data de nascimento é obrigatório!' } //obrigatorio
+    ],
+    genero: [
+      { tipo: 'required', mensagem: 'O campo gênero é obrigatório!' } //obrigatorio
+    ],
+    celular: [
+      { tipo: 'maxlength', mensagem: 'O campo celular deve ter no máximo 16 caracteres!' } //max 16
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório!' }, //obrigatorio
+      { tipo: 'email', mensagem: 'E-mail inválido!' } //INVÁLIDO
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'O campo senha é obrigatório!' }, //obrigatório
+      { tipo: 'minlength', mensagem: 'O campo senha deve ter no mínimo 6 caracteres!' } //min 6
+    ],
+    confirmarSenha: [
+      { tipo: 'required', mensagem: 'O campo confirmar senha é obrigatório!' }, //obrigatório
+      { tipo: 'minlength', mensagem: 'O campo confirmar senha deve ter no mínimo 6 caracteres!' } //min 6
+    ],
+  }
+
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { 
+    this.formRegistro = formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])], //Nome: [obrigatório, minímo(3)]
+      cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14)])],//CPF:[obrigatório, minímo(11), máximo(14)]
+      nascimento: ['', Validators.compose([Validators.required])], //Data de Nascimento: [obrigatório]  
+      genero: ['', Validators.compose([Validators.required])], //Genero: [obrigatório]
+      celular: ['', Validators.compose([Validators.maxLength(16)])], //Celular: [máximo(16)]
+      email: ['', Validators.compose([Validators.required, Validators.email])], //E-mail: [obrigatório]
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])], //Senha: [obrigatório, minímo(6)]
+      confirmarSenha: ['', Validators.compose([Validators.required, Validators.minLength(6)])], //Confirma Senha: [obrigatório, minímo(6)]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  public registro() {
+    if(this.formRegistro.valid){
+      console.log('Formulário Válido!');
+      this.router.navigateByUrl("/login");
+    }else {
+      console.log('Formulário inválido.');
+    }
   }
 
 }
